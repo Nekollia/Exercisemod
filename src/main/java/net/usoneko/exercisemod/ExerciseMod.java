a52f3ea5864f152a19335238471d702d28e83fad
嘘猫 <@DESKTOP-IPS046J>
2024-02-28 11:39:38 +0900

    add sapphire

--- src/main/java/net/usoneko/exercisemod/ExerciseMod.java
+++ src/main/java/net/usoneko/exercisemod/ExerciseMod.java
@@ -1,17 +1,11 @@
 package net.usoneko.exercisemod;
 
+import org.slf4j.Logger;
+
 import com.mojang.logging.LogUtils;
+
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -24,10 +18,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-import org.slf4j.Logger;
+import net.usoneko.exercisemod.item.ModItems;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ExerciseMod.MOD_ID)
@@ -41,6 +32,8 @@ public class ExerciseMod
     public ExerciseMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        
+        ModItems.register(modEventBus);
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
@@ -63,7 +56,9 @@ public class ExerciseMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+    	if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+    		event.accept(ModItems.SAPPHIRE);
+    	}
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/usoneko/exercisemod/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.usoneko.exercisemod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.usoneko.exercisemod.ExerciseMod;
+
+public class ModItems {
+	public static final DeferredRegister<Item> ITEMS = 
+			DeferredRegister.create(ForgeRegistries.ITEMS, ExerciseMod.MOD_ID) ;
+	
+	public static final RegistryObject<Item> SAPPHIRE = 
+			ITEMS.register("sapphire", () -> new Item(new Item.Properties()));
+	
+	public static void register(IEventBus eventBus) {
+		ITEMS.register(eventBus);
+	}
+
+}
